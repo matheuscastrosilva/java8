e1af893d63b5e37a43aabc66da366fe9b00eda84
matheus.castro <matheus.castro@basis.com.br>
2017-05-18 14:12:24 -0300

    PROJETO JUNIT E MOCKITO TESTES

---
+++ teste-de-unidade/test/br/com/caelum/leilao/servico/EncerradorDeLeilaoTeste.java
@@ -0,0 +1,22 @@
+package br.com.caelum.leilao.servico;
+
+import br.com.caelum.leilao.builder.CriadorDeLeilao;
+import br.com.caelum.leilao.dominio.Leilao;
+import org.junit.Test;
+
+import java.util.Calendar;
+
+/**
+ * Created by matheus on 18/05/17.
+ */
+public class EncerradorDeLeilaoTeste {
+
+    @Test
+    public void deveEncerrarLeiloesQueComecaramUmaSemanaAntes() {
+        Calendar antiga = Calendar.getInstance();
+        antiga.set(1999, 1, 20);
+
+        Leilao leilao = new CriadorDeLeilao().para("TV de plasma").naData(antiga).constroi();
+        Leilao leilao2 = new CriadorDeLeilao().para("Geladeira").naData(antiga).constroi();
+    }
+}
